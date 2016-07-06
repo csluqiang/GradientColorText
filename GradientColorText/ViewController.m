dfdb82697df3f908d51aee80df08c9a750383c0b
卢强 <qiang.lu@renren-inc.com>
2016-07-06 10:27:07 +0800

    Initial Commit

---
+++ GradientColorText/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GradientColorText
+//
+//  Created by renren on 16/7/6.
+//  Copyright © 2016年 lasso. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
